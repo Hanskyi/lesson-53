c31bf51a8ca42e5ddf65ee0db62e66155f4c4f30
Kangeldi Eginbaev <han.eginbaev@gmail.com>
2023-05-16 22:16:16 +0600

    fixed the logic

--- Документы/HomeWork-50/components-demo/src/Number/Number.tsx
+++ Документы/HomeWork-50/components-demo/src/Number/Number.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
 
-
-
 const Number = () => {
 
+    const randomNumbers: JSX.Element[] = [];
+    const randomNumberSave: number[] = [];
 
-const  randomNumbers:JSX.Element[] = []
+    while (randomNumberSave.length < 5) {
+        const randomNumber: number = Math.floor(Math.random() * (36 - 5 + 1)) + 5;
 
-
-
-        for( let i:number = 0; i < 5; i++) {
-            const randomNumber: number = Math.floor(Math.random() * (36 - 5 + 1)) + 5;
-            randomNumbers.push (
-                <div className="random-number">{randomNumber}</div>
-            )
+        if (!randomNumberSave.includes(randomNumber)) {
+            randomNumberSave.push(randomNumber);
         }
+    }
 
-    return <>{randomNumbers}</>
+    randomNumberSave.sort((a, b) => a - b);
 
-
+    for (let i: number = 0; i < 5; i++) {
+        randomNumbers.push(
+            <div key={i} className={`random-number random-number-${i}`}> {randomNumberSave[i]}</div>
+        );
+        console.log(randomNumberSave);
+    }
+    return <>{randomNumbers}</>;
 }
 
 export default Number;
