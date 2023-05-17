decb49619a57ff916214a990be476122a40f3afa
Kangeldi Eginbaev <han.eginbaev@gmail.com>
2023-05-17 21:51:42 +0600

    final project

--- Документы/HomeWork-50/components-demo/src/App.tsx
+++ Документы/HomeWork-50/components-demo/src/App.tsx
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Number from "./Number/Number";
 
-const App = () => (
-    <div className="App">
-        <div className="container">
-            <button className="btn">New numbers</button>
-            <div className="number-container">
-                < Number />
+const App = () => {
+    const [numbers, setNumbers] = useState<number[]>([]);
+
+    const generateNewNumbers = () => {
+        setNumbers([]);
+        const newNumbers: number[] = [];
+
+        while (newNumbers.length < 5) {
+            const randomNumber: number = Math.floor(Math.random() * (36 - 5 + 1)) + 5;
+
+            if (!newNumbers.includes(randomNumber)) {
+                newNumbers.push(randomNumber);
+            }
+        }
+
+        newNumbers.sort((a, b) => a - b);
+        setNumbers(newNumbers);
+    };
+
+    return (
+        <div className="App">
+            <div className="container">
+                <button onClick={generateNewNumbers} className="btn">New numbers</button>
+                <div className="number-container">
+                    <Number numbers={numbers} />
+                </div>
             </div>
         </div>
-    </div>
-);
-
+    );
+};
 export default App;
--- Документы/HomeWork-50/components-demo/src/Number/Number.tsx
+++ Документы/HomeWork-50/components-demo/src/Number/Number.tsx
@@ -1,27 +1,21 @@
 import React from 'react';
 
-const Number = () => {
-
-    const randomNumbers: JSX.Element[] = [];
-    const randomNumberSave: number[] = [];
-
-    while (randomNumberSave.length < 5) {
-        const randomNumber: number = Math.floor(Math.random() * (36 - 5 + 1)) + 5;
-
-        if (!randomNumberSave.includes(randomNumber)) {
-            randomNumberSave.push(randomNumber);
-        }
-    }
-
-    randomNumberSave.sort((a, b) => a - b);
-
-    for (let i: number = 0; i < 5; i++) {
-        randomNumbers.push(
-            <div key={i} className={`random-number random-number-${i}`}> {randomNumberSave[i]}</div>
-        );
-        console.log(randomNumberSave);
-    }
-    return <>{randomNumbers}</>;
+interface NumberProps {
+    numbers: number[];
 }
+
+const Number: React.FC<NumberProps> = ({ numbers }) => {
+    const numberElements: JSX.Element[] = [];
+
+    for (let i = 0; i < numbers.length; i++) {
+        numberElements.push(
+            <div key={i} className="random-number">
+                {numbers[i]}
+            </div>
+        );
+    }
+
+    return <>{numberElements}</>;
+};
 
 export default Number;
